ff77f0f90c0580d26705896bd3ac8810cb6d86ba
Christian Hujer <cher@riedquat.de>
2015-03-12 03:01:31 +0530

    Add missing include shield.

--- example/ffile.h
+++ example/ffile.h
@@ -1,3 +1,6 @@
+#ifndef FFILE_H
+#define FFILE_H
+
 #include <stdio.h>
 
 #define l_withFilesOrStdin(paths, ...) do { withFilesOrStdin(paths, l((FILE *file, const char *path), __VA_ARGS__ )); } while (0)
@@ -6,3 +9,5 @@ typedef void (*processFile)(FILE *, const char *);
 
 extern void withFile(const char *path, const char *mode, processFile);
 extern void withFilesOrStdin(char *const paths[], processFile);
+
+#endif
